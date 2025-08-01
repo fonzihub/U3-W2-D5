7defd127ed0e1534ebd438ab7adf85f20ec62101
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-08-01 10:08:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyNavbar from "./Components/MyNavbar";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MyNavbar.jsx
@@ -0,0 +1,39 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const MyNavbar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">EpiMeteo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Previsioni</Nav.Link>
+
+            <Nav.Link href="#">Situazione</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Cerca località"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Cerca</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
